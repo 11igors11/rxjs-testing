abb60520a68c28fa6faac8a033e6d6abcba214d5
user <user@user.com>
2018-07-22 19:56:57 +0300

    Initial setup

---
+++ src/range.spec.js
@@ -0,0 +1,18 @@
+import {cold, hot} from 'jasmine-marbles';
+import * as Observable from "rxjs";
+
+describe("Range", () => {
+
+    it("Emits expected values", () => {
+
+        const values = {a: 1, b: 2, c: 3};
+
+        const expected = cold(`(abc|)`, values);
+
+        const result = Observable.range(1, 3);
+
+        expect(result).toBeObservable(expected);
+
+    });
+
+});
